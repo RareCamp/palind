926b1764f7963e377e742ffac74d4fbfe035151c
Víctor López Ferrando <victor@iomob.net>
2023-08-04 16:19:48 +0200

    Add dice score to linker

--- linkage/linker.py
+++ linkage/linker.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 
+def dice(a, b) -> float:
+    assert len(a) == len(b)
+    assert len(a) > 0
+    a = np.array(list(map(int, list(a))))
+    b = np.array(list(map(int, list(b))))
+    return 2 * (a & b).sum() / (a.sum() + b.sum())
+
+
 def similarity(a, b) -> float:
     assert len(a) == len(b)
     assert len(a) > 0
