d8f14b4733e79462d829e3ea3546d08df65d2694
Víctor López Ferrando <victor@iomob.net>
2024-01-09 12:04:56 +0100

    Use production config in App runner

---
+++ backend/curesdev/production.py
@@ -0,0 +1,18 @@
+import os
+
+from .settings import *
+
+DEBUG = False
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = ["localhost"]
+CSRF_TRUSTED_ORIGINS = ["https://ymbdp7523p.us-east-1.awsapprunner.com/"]
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
--- backend/startup.sh
+++ backend/startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export DJANGO_SETTINGS_MODULE=curesdev.production
 python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 curesdev.wsgi
