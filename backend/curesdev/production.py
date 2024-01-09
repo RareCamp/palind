9055acb41cf365b087cfc6b1c8c9ca04b030addc
Víctor López Ferrando <victor@iomob.net>
2024-01-09 17:02:31 +0100

    Enabled some production settings

--- backend/curesdev/production.py
+++ backend/curesdev/production.py
@@ -4,15 +4,15 @@ from .settings import *
 
 DEBUG = True
 
-#SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+# SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-#ALLOWED_HOSTS = ["localhost", "ymbdp7523p.us-east-1.awsapprunner.com", "*"]
-#CSRF_TRUSTED_ORIGINS = ["https://ymbdp7523p.us-east-1.awsapprunner.com/"]
+# ALLOWED_HOSTS = ["localhost", "ymbdp7523p.us-east-1.awsapprunner.com", "*"]
+# CSRF_TRUSTED_ORIGINS = ["https://ymbdp7523p.us-east-1.awsapprunner.com/"]
 
-#SECURE_SSL_REDIRECT = True
-#SESSION_COOKIE_SECURE = True
-#CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-#SECURE_HSTS_SECONDS = 60
-#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-#SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
