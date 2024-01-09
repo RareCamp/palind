e39e67dda843a9121395cf29853c0fb695459a8d
Víctor López Ferrando <victor@iomob.net>
2024-01-09 17:08:03 +0100

    Change production settings

--- backend/curesdev/production.py
+++ backend/curesdev/production.py
@@ -9,9 +9,9 @@ DEBUG = True
 # ALLOWED_HOSTS = ["localhost", "ymbdp7523p.us-east-1.awsapprunner.com", "*"]
 # CSRF_TRUSTED_ORIGINS = ["https://ymbdp7523p.us-east-1.awsapprunner.com/"]
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+#SECURE_SSL_REDIRECT = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 
 SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
